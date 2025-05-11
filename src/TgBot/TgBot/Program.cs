4e438ce92c04bd183d2c3a1b12cf7d195fa67620
IvanKonovalenko <ivangame67@gmail.com>
2025-05-11 14:12:25 +0300

    tgbot

--- src/TgBot/TgBot/Program.cs
+++ src/TgBot/TgBot/Program.cs
@@ -1,21 +1,66 @@
-﻿using Telegram.Bot;
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 
+
+string json = await File.ReadAllTextAsync("appsettings.json");
+using var doc = JsonDocument.Parse(json);
+string token = doc.RootElement.GetProperty("BotToken").GetString();
+
+
 using var cts = new CancellationTokenSource();
-var bot = new TelegramBotClient("YOUR_BOT_TOKEN", cancellationToken: cts.Token);
+var bot = new TelegramBotClient(token, cancellationToken: cts.Token);
 var me = await bot.GetMe();
+bot.OnError += OnError;
 bot.OnMessage += OnMessage;
+bot.OnUpdate += OnUpdate;
 
-Console.WriteLine($"@{me.Username} is running... Press Enter to terminate");
-Console.ReadLine();
-cts.Cancel(); // stop the bot
+while (true) { }
 
-// method that handle messages received by the bot:
+
 async Task OnMessage(Message msg, UpdateType type)
 {
-    if (msg.Text is null) return;	// we only handle Text messages here
-    Console.WriteLine($"Received {type} '{msg.Text}' in {msg.Chat}");
-    // let's echo back received text in the chat
-    await bot.SendMessage(msg.Chat, $"{msg.From} said: {msg.Text}");
+    if (msg.Text == "/start")
+    {
+        await bot.SendMessage(msg.Chat, "Отпрвьте видео для проверки на наличие deepfake");
+    }
+    else if (msg.Video != null)
+    { 
+        await using var fileStream = new MemoryStream();
+        var tgFile = await bot.GetInfoAndDownloadFile(msg.Video.FileId, fileStream);
+        var result = await SendVideoToBackend(fileStream);
+        await bot.SendMessage(msg.Chat, result);
+    }
+    else
+    {
+        await bot.SendMessage(msg.Chat, "Вы должны отправить видео");
+    }
+}
+async Task<string> SendVideoToBackend(MemoryStream videoStream)
+{
+    using (var client = new HttpClient())
+    {
+        var form = new MultipartFormDataContent();
+
+        var fileContent = new StreamContent(videoStream);
+        fileContent.Headers.ContentType = new MediaTypeHeaderValue("video/mp4"); 
+
+        form.Add(fileContent, "file", "video.mp4");
+
+        var response = await client.PostAsync("http://backend:8080/upload", form);
+        var responseBody = await response.Content.ReadAsStringAsync();
+        return responseBody; 
+       
+    }
+}
+async Task OnError(Exception exception, HandleErrorSource source)
+{
+    Console.WriteLine(exception);
+}
+async Task OnUpdate(Update update)
+{
+   
 }
